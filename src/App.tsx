65b6c240fddf3b6c814165ffbb37802023a8dba5
hamo-o <yheel369@gmail.com>
2025-01-20 14:52:38 +0900

    chore: styled component 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import styled from "styled-components";
+
 function App() {
 
   return (
-    <div>
+    <Title>
       반갑습니다
-    </div>
+    </Title>
   )
 }
 
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: #BF4F74;
+`;
+
 export default App
+
