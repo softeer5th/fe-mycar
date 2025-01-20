ef14dd8a5035000bae023d9e2ae6912148a3a620
hamo-o <yheel369@gmail.com>
2025-01-20 17:10:21 +0900

    design: 기본 디자인 시스템

---
+++ src/theme/color.ts
@@ -0,0 +1,9 @@
+export const color = {
+    "surface-dark": "#E5DCD3",
+    "surface-default": "#FFFFFF",
+    "surface-alt": "#F1ECE8",
+    "border": "#E9EAEA",
+    "highlight": "#0580A9",
+    "text-default": "#151515",
+    "text-sub": "#9E9D9D",   
+}
