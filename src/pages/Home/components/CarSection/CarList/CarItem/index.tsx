104f9e212215eada46304fd7363be20a5f80b37e
rbgksqkr <rbgks1937@naver.com>
2025-01-21 12:21:09 +0900

    refactor: 자동차 가격 만원 단위로 변환해주는 유틸함수 구현

--- src/pages/Home/components/CarSection/CarList/CarItem/index.tsx
+++ src/pages/Home/components/CarSection/CarList/CarItem/index.tsx
@@ -11,12 +11,18 @@ interface CarItemProps {
   car: Car;
 }
 
+function formatTenThousandUnit(num: number) {
+  const tenThousandUnit = (num / 10000).toFixed(2);
+
+  return Number(tenThousandUnit).toLocaleString('ko-KR');
+}
+
 const CarItem = ({ car }: CarItemProps) => {
   return (
     <S.CarItemLayout>
       <img src={car.imgSrc} alt="현대차" />
       <S.Name>{car.name}</S.Name>
-      <S.Price>{car.price}</S.Price>
+      <S.Price>{formatTenThousandUnit(car.price)}만원</S.Price>
       <span>링크</span>
     </S.CarItemLayout>
   );
