ea9bcca628be7122cb494549a82084195853d0e1
rbgksqkr <rbgks1937@naver.com>
2025-01-21 14:53:03 +0900

    chore: msw 초기 설정

--- src/main.tsx
+++ src/main.tsx
@@ -6,15 +6,27 @@ import GlobalStyle from './styles/GlobalStyle.tsx';
 import theme from './styles/theme.ts';
 import { BrowserRouter, Routes, Route } from 'react-router';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
-  </StrictMode>,
-);
+const enableMocking = async () => {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
+
+  const { worker } = await import('./mocks/browser');
+
+  return await worker.start();
+};
+
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </StrictMode>,
+  );
+});
