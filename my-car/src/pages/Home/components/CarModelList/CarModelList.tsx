1c6653e55daae71eb877c60aea4476f38990f60d
vavoya <vavoya6324@gmail.com>
2025-01-21 14:48:20 +0900

    refactor: 홈 화면 컴포넌트 분리 - Home, Banner, CarList, TabMenu 로 분리 - 반복적인 컴포넌트는 map 으로 처리 - 버튼 색 변경 기능 추가

---
+++ my-car/src/pages/Home/components/CarModelList/CarModelList.tsx
@@ -0,0 +1,92 @@
+import {
+    CarAmount,
+    CarBadge,
+    CarImg,
+    CarImgWrapper,
+    CarModel,
+    CarModelGrid,
+    CarName,
+    CarText
+} from "./CarModelList.style.ts";
+import {useEffect, useState} from "react";
+import {carModelSelectionType} from "../../Home.tsx";
+
+type CarModelItemType = {
+    src: string,
+    name: string,
+    amount: string,
+    badge: string,
+}
+
+export default function CarModelList({carModelSelection}: {carModelSelection: carModelSelectionType}) {
+    const [carModelItems, setCarModelItems] = useState<CarModelItemType[]>([
+        {
+            src: "https://www.hyundai.com/contents/repn-car/side-45/kona-electric-24my-45side.png",
+            name: "캐스퍼 일렉트릭1",
+            amount: "2,999만원~",
+            badge: "캐스퍼 온라인 바로가기",
+        },
+        {
+            src: "https://www.hyundai.com/contents/repn-car/side-45/kona-electric-24my-45side.png",
+            name: "캐스퍼 일렉트릭2",
+            amount: "2,999만원~",
+            badge: "캐스퍼 온라인 바로가기",
+        },
+        {
+            src: "https://www.hyundai.com/contents/repn-car/side-45/kona-electric-24my-45side.png",
+            name: "캐스퍼 일렉트릭3",
+            amount: "2,999만원~",
+            badge: "캐스퍼 온라인 바로가기",
+        },
+        {
+            src: "https://www.hyundai.com/contents/repn-car/side-45/kona-electric-24my-45side.png",
+            name: "캐스퍼 일렉트릭4",
+            amount: "2,999만원~",
+            badge: "캐스퍼 온라인 바로가기",
+        },
+        {
+            src: "https://www.hyundai.com/contents/repn-car/side-45/kona-electric-24my-45side.png",
+            name: "캐스퍼 일렉트릭5",
+            amount: "2,999만원~",
+            badge: "캐스퍼 온라인 바로가기",
+        },
+
+    ]);
+
+    useEffect(() => {
+        /*
+        fetch().then
+         */
+    }, [carModelSelection]);
+
+
+    return (
+        <CarModelGrid>
+            {
+                carModelItems.map((carModelItem) => {
+                    const {src, name, amount, badge} = carModelItem;
+                    return (
+                        <CarModelListItem key={name} src={src} name={name} amount={amount} badge={badge}/>
+                    )
+                })
+            }
+        </CarModelGrid>
+    )
+}
+
+
+function CarModelListItem({src, name, amount, badge}: {src: string; name: string; amount: string, badge: string}) {
+    return (
+        <CarModel>
+            <CarImgWrapper>
+                <CarImg src={src}></CarImg>
+            </CarImgWrapper>
+
+            <CarText>
+                <CarName>{name}</CarName>
+                <CarAmount>{amount}</CarAmount>
+                <CarBadge>{badge}</CarBadge>
+            </CarText>
+        </CarModel>
+    )
+}
---
+++ my-car/src/pages/Home/components/HomeBanner/HomeBanner.style.ts
@@ -0,0 +1,28 @@
+import { styled } from "styled-components";
+// title style
+export const PageTitleWrapper = styled.section`
+  width: 100%;
+  height: 360px;
+  padding: 90px 60px 0px 60px;
+  background-color: rgb(228, 220, 211);
+`;
+
+export const PageTitle = styled.div`
+  height: 100%;
+  background-color: rgba(246, 243, 242, 0.7);
+  display: flex;
+  justify-content: flex-start;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 80px;
+`;
+
+export const Title = styled.h1`
+  font-size: 58px;
+  line-height: 51px;
+`;
+
+export const Detail = styled.p`
+  margin-top: 40px;
+  font-weight: 500;
+`;
