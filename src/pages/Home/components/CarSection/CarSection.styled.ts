4e52456d7160c2ff05715740a6b4821cd39750bc
rbgksqkr <rbgks1937@naver.com>
2025-01-21 12:23:33 +0900

    design: 색상 디자인 토큰으로 수정

--- src/pages/Home/components/CarSection/CarSection.styled.ts
+++ src/pages/Home/components/CarSection/CarSection.styled.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
 
 export const CarSectionLayout = styled.section`
-  border-top: 0.1rem solid #e5e5e5;
+  border-top: 0.1rem solid ${({ theme }) => theme.colors.gray300};
 `;
