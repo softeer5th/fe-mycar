7245b98c5401517919cfd0b2c6723c7e83bd2b6e
kimdongyoung <wwweric12@naver.com>
2025-01-22 19:00:20 +0900

    feat: 모델 선택 페이지 UI 구현

--- src/pages/select-car-model.tsx
+++ src/pages/select-car-model.tsx
@@ -1,5 +1,23 @@
+import CarProductCard from '../components/card/car-product-card';
+
 const SelectCarModel = () => {
-  return <div>SelectCarModel</div>;
+  const productCount = 10;
+
+  const handleButtonClick = () => {};
+
+  return (
+    <div className="w-full p-[50px]">
+      <div className=" ml-[5px]  mb-[10px] text-[16px] leading-[30px] ">
+        전체 모델
+        <span className="text-[#007fa8]">({productCount})</span>
+      </div>
+      <div className=" min-w-[1340px]">
+        <div className="flex gap-[30px] w-[1340px]">
+          <CarProductCard handleButtonClick={() => handleButtonClick()} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SelectCarModel;
