c737eec70910c6c5aa893bff86562dd012ed2710
MinGyo Kim <110979416+DrCloy@users.noreply.github.com>
2025-01-22 15:37:20 +0900

    refactor: Repository 로직 수정

--- src/repository/car-type-json.ts
+++ src/repository/car-type-json.ts
@@ -1,9 +1,19 @@
 import { CarType } from '../core/model';
 import { CarTypeRepository } from '../core/repository';
 
+type CarTypeJson = {
+  carTypeCode: string;
+  carTypeName: string;
+};
+
 export default class CarTypeJsonRepository implements CarTypeRepository {
   async getCarTypes(): Promise<CarType[]> {
     const response = await fetch('http://localhost:3000/carTypeList');
-    return response.json();
+    const carTypes = await response.json();
+
+    return carTypes.map((carType: CarTypeJson) => ({
+      carTypeCode: carType.carTypeCode,
+      carTypeName: carType.carTypeName,
+    }));
   }
 }
