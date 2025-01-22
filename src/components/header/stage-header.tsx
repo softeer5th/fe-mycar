776a89e19357664c308e2d77174865ea9886e8c9
kimdongyoung <wwweric12@naver.com>
2025-01-22 19:02:24 +0900

    feat: 주소에 따라 stageHeader 선택 스타일 구현

--- src/components/header/stage-header.tsx
+++ src/components/header/stage-header.tsx
@@ -4,25 +4,30 @@ const StageHeader = () => {
   const { carCode } = useParams();
   const { pathname } = useLocation();
 
+  const selectedIcon =
+    'after:absolute  after:bottom-[-23px] after:left-[60px]  after:h-[25px] after:border-b-white after:border-b-[10px]  after:border-r-transparent after:border-r-[10px] after:border-l-transparent after:border-l-[10px]';
+
   return (
     <div className="  relative w-full px-[50px] h-[78px] bg-[#e4dcd3] z-10 ">
       <ul className=" flex items-center absolute bottom-0 left-[50px]     ">
-        <li className=" relative h-[49px] text-[20px] ">
+        <li className="relative h-[49px] text-[20px]">
           <Link
             to={`/model/${carCode}`}
             className={`${
-              pathname.includes('making') ? ' opacity-50  ' : ''
-            } border-r border-black pr-[50px] `}
+              pathname.includes('making') ? ' opacity-50 ' : selectedIcon
+            } border-r border-black pr-[50px] relative `}
           >
-            <span className=" mr-5">01</span> 모델선택
+            <span className=" mr-5">01</span> 모델 선택
           </Link>
         </li>
-        <li className=" relative h-[49px] text-[20px] ">
+        <li className="relative h-[49px] text-[20px]">
           <Link
             to={`/model/${carCode}/making`}
             className={`${
-              pathname.includes('making') ? ` ` : 'opacity-50'
-            } pl-[50px]`}
+              pathname.includes('making')
+                ? `${selectedIcon} after:left-[120px]`
+                : 'opacity-50'
+            } pl-[50px] relative `}
           >
             <span className=" mr-5">02 </span> 내 차 만들기
           </Link>
