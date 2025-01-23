b4ddf2938231953a294f10b7aac9e47b0999f2a6
vavoya <vavoya6324@gmail.com>
2025-01-23 15:36:22 +0900

    feat: msw 초기 설정 및 handler 정의
    - 홈페이지 데이터 전송
    - 모델 선택 데이터 전송

---
+++ my-car/src/mocks/data/carModelSelection.ts
@@ -0,0 +1,236 @@
+const HYUNDAI_URL = `https://www.hyundai.com`
+
+export default {
+    "1": {
+        carId : 1,
+        trimList: [
+            {
+                trimType: '구동방식',
+                trimName: ['2WD','4WD']
+            },
+        ],
+        carModelItems : [
+            {
+                carType: 'E-LITE(롱레인지) 18인치',
+                carPrice: '55,770,000 원',
+                carImg: `${HYUNDAI_URL}/contents/vr360/CE02/trim/VL.png`,
+                carDetailType: '4WD',
+                carImgSourceList: [
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VL-USP-001.png`,
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VL-USP-002.png`,
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VL-USP-003.png`
+                ],
+                carBadge: '',
+                trimTypeNumber: [1]
+            },
+            {
+                carType: 'Exclusive(롱레인지) 18인치',
+                carPrice: '59,400,000 원',
+                carImg: `${HYUNDAI_URL}/contents/vr360/CE02/trim/VB.png`,
+                carDetailType: '4WD',
+                carImgSourceList: [
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VB-USP-001.png`,
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VB-USP-002.png`,
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VB-USP-003.png`
+                ],
+                carBadge: '',
+                trimTypeNumber: [1]
+            },
+            {
+                carType: 'Exclusive(롱레인지) 20인치',
+                carPrice: '59,990,000 원',
+                carImg: `${HYUNDAI_URL}/contents/vr360/CE02/trim/VC.png`,
+                carDetailType: '4WD',
+                carImgSourceList: [
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VC-USP-001.png`,
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VC-USP-002.png`,
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VC-USP-003.png`
+                ],
+                carBadge: '',
+                trimTypeNumber: [1]
+            },
+            {
+                carType: 'Exclusive+(롱레인지) 18인치',
+                carPrice: '61,190,000 원',
+                carImg: `${HYUNDAI_URL}/contents/vr360/CE02/trim/VD.png`,
+                carDetailType: '4WD',
+                carImgSourceList: [
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VD-USP-001.png`,
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VD-USP-002.png`,
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VD-USP-003.png`
+                ],
+                carBadge: '',
+                trimTypeNumber: [1]
+            },
+            {
+                carType: 'Exclusive+(롱레인지) 20인치',
+                carPrice: '61,780,000 원',
+                carImg: `${HYUNDAI_URL}/contents/vr360/CE02/trim/VE.png`,
+                carDetailType: '4WD',
+                carImgSourceList: [
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VE-USP-001.png`,
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VE-USP-002.png`,
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VE-USP-003.png`
+                ],
+                carBadge: '',
+                trimTypeNumber: [1]
+            },
+            {
+                carType: 'Prestige(롱레인지) 18인치',
+                carPrice: '64,980,000 원',
+                carImg: `${HYUNDAI_URL}/contents/vr360/CE02/trim/VF.png`,
+                carDetailType: '4WD',
+                carImgSourceList: [
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VF-USP-001.png`,
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VF-USP-002.png`,
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VF-USP-003.png`
+                ],
+                carBadge: '',
+                trimTypeNumber: [1]
+            },
+            {
+                carType: 'Prestige(롱레인지) 20인치',
+                carPrice: '65,570,000 원',
+                carImg: `${HYUNDAI_URL}/contents/vr360/CE02/trim/VG.png`,
+                carDetailType: '4WD',
+                carImgSourceList: [
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VG-USP-001.png`,
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VG-USP-002.png`,
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VG-USP-003.png`
+                ],
+                carBadge: 'Best',
+                trimTypeNumber: [1]
+            },
+            {
+                carType: 'E-Value +(스탠다드) 18인치',
+                carPrice: '49,450,000 원',
+                carImg: `${HYUNDAI_URL}/contents/vr360/CE02/trim/VH.png`,
+                carDetailType: '2WD',
+                carImgSourceList: [
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VH-USP-001.png`,
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VH-USP-002.png`,
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VH-USP-003.png`
+                ],
+                carBadge: 'Best',
+                trimTypeNumber: [0]
+            },
+            {
+                carType: 'Exclusive(스탠다드) 18인치',
+                carPrice: '52,090,000 원',
+                carImg: `${HYUNDAI_URL}/contents/vr360/CE02/trim/VA.png`,
+                carDetailType: '2WD',
+                carImgSourceList: [
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VA-USP-001.png`,
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VA-USP-002.png`,
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VA-USP-003.png`
+                ],
+                carBadge: '',
+                trimTypeNumber: [0]
+            },
+            {
+                carType: 'E-LITE(롱레인지) 18인치',
+                carPrice: '53,300,000 원',
+                carImg: `${HYUNDAI_URL}/contents/vr360/CE02/trim/VL.png`,
+                carDetailType: '2WD',
+                carImgSourceList: [
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VL-USP-001.png`,
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VL-USP-002.png`,
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VL-USP-003.png`
+                ],
+                carBadge: '',
+                trimTypeNumber: [0]
+            },
+            {
+                carType: 'Exclusive+(스탠다드) 18인치',
+                carPrice: '53,880,000 원',
+                carImg: `${HYUNDAI_URL}/contents/vr360/CE02/trim/VK.png`,
+                carDetailType: '2WD',
+                carImgSourceList: [
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VK-USP-001.png`,
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VK-USP-002.png`,
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VK-USP-003.png`
+                ],
+                carBadge: '',
+                trimTypeNumber: [0]
+            },
+            {
+                carType: 'Exclusive(롱레인지) 18인치',
+                carPrice: '56,930,000 원',
+                carImg: `${HYUNDAI_URL}/contents/vr360/CE02/trim/VB.png`,
+                carDetailType: '2WD',
+                carImgSourceList: [
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VB-USP-001.png`,
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VB-USP-002.png`,
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VB-USP-003.png`
+                ],
+                carBadge: '',
+                trimTypeNumber: [0]
+            },
+            {
+                carType: 'Exclusive(롱레인지) 20인치',
+                carPrice: '57,520,000 원',
+                carImg: `${HYUNDAI_URL}/contents/vr360/CE02/trim/VC.png`,
+                carDetailType: '2WD',
+                carImgSourceList: [
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VC-USP-001.png`,
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VC-USP-002.png`,
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VC-USP-003.png`
+                ],
+                carBadge: '',
+                trimTypeNumber: [0]
+            },
+            {
+                carType: 'Exclusive+(롱레인지) 18인치',
+                carPrice: '58,720,000 원',
+                carImg: `${HYUNDAI_URL}/contents/vr360/CE02/trim/VD.png`,
+                carDetailType: '2WD',
+                carImgSourceList: [
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VD-USP-001.png`,
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VD-USP-002.png`,
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VD-USP-003.png`
+                ],
+                carBadge: '',
+                trimTypeNumber: [0]
+            },
+            {
+                carType: 'Exclusive+(롱레인지) 20인치',
+                carPrice: '59,310,000 원',
+                carImg: `${HYUNDAI_URL}/contents/vr360/CE02/trim/VE.png`,
+                carDetailType: '2WD',
+                carImgSourceList: [
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VE-USP-001.png`,
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VE-USP-002.png`,
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VE-USP-003.png`
+                ],
+                carBadge: '',
+                trimTypeNumber: [0]
+            },
+            {
+                carType: 'Prestige(롱레인지) 18인치',
+                carPrice: '62,510,000 원',
+                carImg: `${HYUNDAI_URL}/contents/vr360/CE02/trim/VF.png`,
+                carDetailType: '2WD',
+                carImgSourceList: [
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VF-USP-001.png`,
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VF-USP-002.png`,
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VF-USP-003.png`
+                ],
+                carBadge: '',
+                trimTypeNumber: [0]
+            },
+            {
+                carType: 'Prestige(롱레인지) 20인치',
+                carPrice: '63,100,000 원',
+                carImg: `${HYUNDAI_URL}/contents/vr360/CE02/trim/VG.png`,
+                carDetailType: '2WD',
+                carImgSourceList: [
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VG-USP-001.png`,
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VG-USP-002.png`,
+                    `${HYUNDAI_URL}/contents/vr360/CE02/trim/VG-USP-003.png`
+                ],
+                carBadge: '',
+                trimTypeNumber: [0]
+            }
+        ]
+    }
+}
