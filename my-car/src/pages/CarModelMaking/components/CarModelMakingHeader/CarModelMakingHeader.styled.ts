4f191cc48a8338f3ff7f25a6107a5b6cc138ac9e
admin <admin@admins-MacBook-Pro-4.local>
2025-01-23 16:57:37 +0900

    feat: 내 차 만들기 페이지 헤더& preview 섹션 구조 설계 및 디자인 진행중

---
+++ my-car/src/pages/CarModelMaking/components/CarModelMakingHeader/CarModelMakingHeader.styled.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  position: fixed;
+  width: 100%;
+  z-index: 5;
+`;
---
+++ my-car/src/pages/CarModelMaking/components/CarModelMakingHeader/CarModelMakingHeader.tsx
@@ -0,0 +1,10 @@
+import StepNavbar from "../StepNavbar/StepNavbar";
+import { Header } from "./CarModelMakingHeader.styled";
+
+export function CarModelMakingHeader() {
+  return (
+    <Header>
+      <StepNavbar />
+    </Header>
+  );
+}
