985b4265dede5a17d925c887928eb04c9e711b0c
elect10 <wlgnsakswp@naver.com>
2025-01-23 12:27:52 +0900

    style: customization style for option page

--- my-car/src/pages/option/Option.tsx
+++ my-car/src/pages/option/Option.tsx
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 // import { Tabbar } from '../../components/header/header.styled';
 import styled from 'styled-components';
 import { PreviewContainer } from './components/PreviewContainer';
+import { CustomizationContainer } from './components/CustomizationContainer';
+
 export default function Option() {
   const { model } = useParams();
 
@@ -13,26 +15,7 @@ export default function Option() {
       <CarMakingContatiner>
         <PreviewContainer />
 
-        {/* <CustomizationContainer>
-          <ColorContainer>
-            <TitleText></TitleText>
-            <ExteriorColorContainer></ExteriorColorContainer>
-            <InteriorColorContainer></InteriorColorContainer>
-          </ColorContainer>
-
-          <OptionContainer>
-            <TitleText></TitleText>
-            <TabbarContainer>
-              <Tabbar></Tabbar>
-              <DynamicContent>
-                <DivisionContainer></DivisionContainer>
-                <DetailProductContainer></DetailProductContainer>
-              </DynamicContent>
-            </TabbarContainer>
-          </OptionContainer>
-
-          <CompleteButton></CompleteButton>
-        </CustomizationContainer> */}
+        <CustomizationContainer />
       </CarMakingContatiner>
     </>
   );
